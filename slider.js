1dad8480e4173c51fbe92d694c0c0021d08a3521
jacksiayh <s10257192@connect.np.edu.sg>
2023-12-01 21:04:12 +0800

    with comments

--- slider.js
+++ slider.js
@@ -1,7 +1,12 @@
+// Initialize slideIndex to keep track of the current slide
 let slideIndex = 0;
+
+// Variable to store the timer for automatic sliding
 let slideTimer;
 
+// Function to display slides
 function showSlides() {
+    // Get all elements with the class "slide"
     const slides = document.getElementsByClassName("slide");
 
     // Check if there are no slides or if slideIndex is out of bounds
@@ -9,14 +14,18 @@ function showSlides() {
         return;
     }
 
+    // Hide all slides
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+    // Display the current slide
     slides[slideIndex].style.display = "block";
 
+    // Increment slideIndex
     slideIndex++;
 
+    // If slideIndex is greater than or equal to the number of slides, reset it to 0
     if (slideIndex >= slides.length) {
         slideIndex = 0;
     }
@@ -26,14 +35,16 @@ function showSlides() {
         clearTimeout(slideTimer);
     }
 
-    // Set a new timer for automatic sliding
+    // Set a new timer for automatic sliding (every 4000 milliseconds or 4 seconds)
     slideTimer = setTimeout(showSlides, 4000);
 }
 
+// Function to navigate to previous or next slides
 function plusSlides(n) {
     showSlides(slideIndex + n);
 }
 
+// Event listener for the DOMContentLoaded event
 document.addEventListener("DOMContentLoaded", () => {
     showSlides(); // Initialize the slider
 });
