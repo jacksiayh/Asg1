8bee64869918ef9c911546120225fd41a64fbff5
jacksiayh <s10257192@connect.np.edu.sg>
2023-12-01 16:33:46 +0800

    truely the final changes

--- slider.js
+++ slider.js
@@ -1,28 +1,39 @@
 let slideIndex = 0;
+let slideTimer;
 
 function showSlides() {
     const slides = document.getElementsByClassName("slide");
+
+    // Check if there are no slides or if slideIndex is out of bounds
+    if (slides.length === 0 || slideIndex < 0 || slideIndex >= slides.length) {
+        return;
+    }
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+    slides[slideIndex].style.display = "block";
+
     slideIndex++;
 
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
     }
 
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change slide every 2 seconds
+    // Clear the previous timer (if any)
+    if (slideTimer) {
+        clearTimeout(slideTimer);
+    }
+
+    // Set a new timer for automatic sliding
+    slideTimer = setTimeout(showSlides, 4000);
 }
 
-
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex + n);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     showSlides(); // Initialize the slider
 });
-
